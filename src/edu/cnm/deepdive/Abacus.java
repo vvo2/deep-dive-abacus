c6de61ceb0664cc9bc6832c9109d45f03f83deaf
vvo2 <vuongkv@gmail.com>
2018-01-23 08:51:52 -0700

    intro to import parse printf

--- src/edu/cnm/deepdive/Abacus.java
+++ src/edu/cnm/deepdive/Abacus.java
@@ -1,13 +1,18 @@
 package edu.cnm.deepdive;
 
+
+import java.util.Arrays;
 public class Abacus {
 
   public static void main(String[] args){
-    double sum = 0;
+    double sum = 0; //primitive are just value, object have address and behavior
     for (String s : args) {
       sum += Double.parseDouble(s); //update a result as sum = sum + s
-    }
-    System.out.println(sum);
+    }         //parsDouble is a name of the method
+  //  System.out.print("Sum = ");
+   // System.out.println(sum);
+    System.out.printf("Sum of %s = %5.2f%n", Arrays.toString(args), sum); //need to import array class
+    //print formatted, %f is decimal placeholder %n is new line
   }
 
 }
